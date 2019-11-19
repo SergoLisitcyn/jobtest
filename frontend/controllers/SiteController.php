abfa9347dc10ea67066c2b8e4e245638374148a9
sergo <sergolisitcyn@gmail.com>
2019-11-20 00:19:51 +0300

    first commit

---
+++ frontend/controllers/SiteController.php
@@ -0,0 +1,322 @@
+<?php
+namespace frontend\controllers;
+
+use common\models\UploadForm;
+use frontend\models\ResendVerificationEmailForm;
+use frontend\models\VerifyEmailForm;
+use Yii;
+use yii\base\InvalidArgumentException;
+use yii\web\BadRequestHttpException;
+use yii\web\Controller;
+use yii\filters\VerbFilter;
+use yii\filters\AccessControl;
+use common\models\LoginForm;
+use frontend\models\PasswordResetRequestForm;
+use frontend\models\ResetPasswordForm;
+use frontend\models\SignupForm;
+use frontend\models\ContactForm;
+use yii\web\UploadedFile;
+use phpQuery;
+/**
+ * Site controller
+ */
+class SiteController extends Controller
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function behaviors()
+    {
+        return [
+            'access' => [
+                'class' => AccessControl::className(),
+                'only' => ['logout', 'signup'],
+                'rules' => [
+                    [
+                        'actions' => ['signup'],
+                        'allow' => true,
+                        'roles' => ['?'],
+                    ],
+                    [
+                        'actions' => ['logout'],
+                        'allow' => true,
+                        'roles' => ['@'],
+                    ],
+                ],
+            ],
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'logout' => ['post'],
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function actions()
+    {
+        return [
+            'error' => [
+                'class' => 'yii\web\ErrorAction',
+            ],
+            'captcha' => [
+                'class' => 'yii\captcha\CaptchaAction',
+                'fixedVerifyCode' => YII_ENV_TEST ? 'testme' : null,
+            ],
+        ];
+    }
+
+    /**
+     * Displays homepage.
+     *
+     * @return mixed
+     */
+    public function actionIndex()
+    {
+        $model = new UploadForm();
+
+        if(Yii::$app->request->post())
+        {
+            $model->file = UploadedFile::getInstance($model, 'file');
+            if ($model->validate()) {
+                $path = Yii::$app->params['pathUploads'];
+                $model->file->saveAs( $path .date('d_m_Y_h_i_s') . '-' . $model->file);
+            }
+        }
+
+        $url = $path .date('d_m_Y_h_i_s') . '-' . $model->file;
+
+        $file = file_get_contents($url);
+
+        $document = phpQuery::newDocumentHTML($file);
+        $documentHtml = $document->find('table tr');
+
+        $i = 0;
+        $data = ['x'=>[],'y'=>[]];
+        foreach ($documentHtml as $rowE) {
+            if($i == 3){
+                $row = pq($rowE);
+                $columns = $row->find('td[class="mspt"]');
+
+                foreach ($columns as $column) {
+                        $colDataP = pq($column)->text();
+                }
+            }
+            if($i > 4){
+                $row = pq($rowE);
+                $columns = $row->find('td');
+                $j = 0;
+                $colData = [];
+                foreach ($columns as $column) {
+                    if($j == 1) {
+                        $colData['date'] = pq($column)->text();
+                    }
+                    if($j == 13) {
+                        $colData['profit'] = pq($column)->text();
+                    }
+                    $j++;
+                }
+                if (isset($colData['date']) && isset($colData['profit'])) {
+                    $data['x'][$i] = $colData['date'];
+                    $data['y'][$i] = $colDataP+$colData['profit'];
+                }
+                $data[] = $colData;
+            }
+            $i++;
+        }
+
+        return $this->render('index', [
+            'model'=>$model,
+            'data'=>$data,
+        ]);
+    }
+
+
+    /**
+     * Logs in a user.
+     *
+     * @return mixed
+     */
+    public function actionLogin()
+    {
+        if (!Yii::$app->user->isGuest) {
+            return $this->goHome();
+        }
+
+        $model = new LoginForm();
+        if ($model->load(Yii::$app->request->post()) && $model->login()) {
+            return $this->goBack();
+        } else {
+            $model->password = '';
+
+            return $this->render('login', [
+                'model' => $model,
+            ]);
+        }
+    }
+
+    /**
+     * Logs out the current user.
+     *
+     * @return mixed
+     */
+    public function actionLogout()
+    {
+        Yii::$app->user->logout();
+
+        return $this->goHome();
+    }
+
+    /**
+     * Displays contact page.
+     *
+     * @return mixed
+     */
+    public function actionContact()
+    {
+        $model = new ContactForm();
+        if ($model->load(Yii::$app->request->post()) && $model->validate()) {
+            if ($model->sendEmail(Yii::$app->params['adminEmail'])) {
+                Yii::$app->session->setFlash('success', 'Thank you for contacting us. We will respond to you as soon as possible.');
+            } else {
+                Yii::$app->session->setFlash('error', 'There was an error sending your message.');
+            }
+
+            return $this->refresh();
+        } else {
+            return $this->render('contact', [
+                'model' => $model,
+            ]);
+        }
+    }
+
+    /**
+     * Displays about page.
+     *
+     * @return mixed
+     */
+    public function actionAbout()
+    {
+        return $this->render('about');
+    }
+
+    public function actionTextHtml()
+    {
+        return $this->render('text-html');
+    }
+    /**
+     * Signs user up.
+     *
+     * @return mixed
+     */
+    public function actionSignup()
+    {
+        $model = new SignupForm();
+        if ($model->load(Yii::$app->request->post()) && $model->signup()) {
+            Yii::$app->session->setFlash('success', 'Thank you for registration. Please check your inbox for verification email.');
+            return $this->goHome();
+        }
+
+        return $this->render('signup', [
+            'model' => $model,
+        ]);
+    }
+
+    /**
+     * Requests password reset.
+     *
+     * @return mixed
+     */
+    public function actionRequestPasswordReset()
+    {
+        $model = new PasswordResetRequestForm();
+        if ($model->load(Yii::$app->request->post()) && $model->validate()) {
+            if ($model->sendEmail()) {
+                Yii::$app->session->setFlash('success', 'Check your email for further instructions.');
+
+                return $this->goHome();
+            } else {
+                Yii::$app->session->setFlash('error', 'Sorry, we are unable to reset password for the provided email address.');
+            }
+        }
+
+        return $this->render('requestPasswordResetToken', [
+            'model' => $model,
+        ]);
+    }
+
+    /**
+     * Resets password.
+     *
+     * @param string $token
+     * @return mixed
+     * @throws BadRequestHttpException
+     */
+    public function actionResetPassword($token)
+    {
+        try {
+            $model = new ResetPasswordForm($token);
+        } catch (InvalidArgumentException $e) {
+            throw new BadRequestHttpException($e->getMessage());
+        }
+
+        if ($model->load(Yii::$app->request->post()) && $model->validate() && $model->resetPassword()) {
+            Yii::$app->session->setFlash('success', 'New password saved.');
+
+            return $this->goHome();
+        }
+
+        return $this->render('resetPassword', [
+            'model' => $model,
+        ]);
+    }
+
+    /**
+     * Verify email address
+     *
+     * @param string $token
+     * @throws BadRequestHttpException
+     * @return yii\web\Response
+     */
+    public function actionVerifyEmail($token)
+    {
+        try {
+            $model = new VerifyEmailForm($token);
+        } catch (InvalidArgumentException $e) {
+            throw new BadRequestHttpException($e->getMessage());
+        }
+        if ($user = $model->verifyEmail()) {
+            if (Yii::$app->user->login($user)) {
+                Yii::$app->session->setFlash('success', 'Your email has been confirmed!');
+                return $this->goHome();
+            }
+        }
+
+        Yii::$app->session->setFlash('error', 'Sorry, we are unable to verify your account with provided token.');
+        return $this->goHome();
+    }
+
+    /**
+     * Resend verification email
+     *
+     * @return mixed
+     */
+    public function actionResendVerificationEmail()
+    {
+        $model = new ResendVerificationEmailForm();
+        if ($model->load(Yii::$app->request->post()) && $model->validate()) {
+            if ($model->sendEmail()) {
+                Yii::$app->session->setFlash('success', 'Check your email for further instructions.');
+                return $this->goHome();
+            }
+            Yii::$app->session->setFlash('error', 'Sorry, we are unable to resend verification email for the provided email address.');
+        }
+
+        return $this->render('resendVerificationEmail', [
+            'model' => $model
+        ]);
+    }
+}
---
+++ frontend/views/site/index.php
@@ -0,0 +1,55 @@
+<?php
+use yii\widgets\ActiveForm;
+use miloschuman\highcharts\Highcharts;
+use kartik\file\FileInput;
+
+/* @var $this yii\web\View */
+
+$this->title = 'Test Job';
+?>
+<div class="site-index">
+    <div class="body-content">
+        <div class="upload" style="width:50%">
+
+            <?php $form = ActiveForm::begin([
+                'options' => ['enctype' => 'multipart/form-data']
+            ]);
+            ?>
+            <?php echo $form->field($model, 'file')->widget(FileInput::classname(), [
+                'options' => ['accept' => 'html/*','multiple' => false],
+                'pluginOptions' => [
+                    'showPreview' => false,
+                    'showCaption' => true,
+                    'showRemove' => true,
+                    'showUpload' => false
+                ]
+            ]);
+            ?>
+            <button class="btn btn-success">Submit</button>
+            <?php ActiveForm::end(); ?>
+        </div>
+        <?php
+        echo Highcharts::widget([
+            'options' => [
+                'title' => ['text' => 'RoboForex (CY) Ltd.'],
+                'xAxis' => [
+                    'categories' => array_map(function($val){
+                    return (float) $val;
+                    },array_values($data['x']))
+                ],
+                'yAxis' => [
+                    'title' => ['text' => 'Profit']
+                ],
+                'series' => [
+                    ['name' => 'Баланс', 'data' => array_map(function($val){
+                        return (float) $val;
+                    },array_values($data['y']))],
+                ]
+            ]
+
+        ]);
+
+
+        ?>
+    </div>
+</div>
